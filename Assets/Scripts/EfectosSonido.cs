a228e2510b5abad79f681f9f3e63be760b541f93
RodriCubilla <111516573+RodriCubilla@users.noreply.github.com>
2022-11-04 11:22:12 -0300

    sound

--- Assets/Scripts/EfectosSonido.cs
+++ Assets/Scripts/EfectosSonido.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class EfectosSonido : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private AudioSource audioSource;
+
+    private void Start() {
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player"))
+        {
+            audioSource.Play();
+        }
     }
 }
